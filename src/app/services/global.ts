ee6411f55456e02cd4443a13cb3085b2d8adc2bf
Jofré Valenzuela <jofre.valenzuela@irdigital.pe>
2020-09-14 16:57:02 -0500

    Modificando estilos

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,4 @@
-export var GLOBAL = {
-  url: 'http://localhost:3977/api/'
-	//  url:'https://api-rest2.herokuapp.com/api/'
-	 
+export const GLOBAL = {
+  url: 'http://ec2-18-219-162-43.us-east-2.compute.amazonaws.com:3977/api/'
+  //  url:'https://api-rest2.herokuapp.com/api/'
 };
